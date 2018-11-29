f496e311c4be4df7490a63cb2522215d1792539c
Mete Ergin <mete.ergin@outlook.com>
2018-11-29 09:06:33 +0300

    First commit

---
+++ general_backend/src/main/java/com/meteergin/GeneralBackendApplication.java
@@ -0,0 +1,12 @@
+package com.meteergin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralBackendApplication.class, args);
+	}
+}
